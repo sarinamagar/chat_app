6e8bfb63c659551fc5093dd43dca65f98134b6a7
sarinamagar <msarina823@gmail.com>
2023-01-25 07:58:29 +0545

    dashboard screen

---
+++ lib/feature/dashboard/widgets/dashboard_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DashboardWidget extends StatefulWidget {
+  const DashboardWidget({super.key});
+
+  @override
+  State<DashboardWidget> createState() => _DashboardWidgetState();
+}
+
+class _DashboardWidgetState extends State<DashboardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
